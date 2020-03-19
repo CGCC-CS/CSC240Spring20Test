2ee3bf0c51174a941371e764c7f59a319316df43
Wade Huber <wadehuber@gmail.com>
2020-03-19 12:44:27 -0700

    Initial test assignment

---
+++ testassignment.scm
@@ -0,0 +1,18 @@
+#lang scheme
+; Write scheme forms to do the following.  Enter your code
+;  directly following the comment it relates to.  You should
+;  not need to define any procedures.
+
+; A Scheme form to add 2 and 10
+
+
+; A Scheme form to determine whichof these things is a symbol or not
+(define thing1 3)
+(define thing2 "CSC240")
+(define thing3 'Arizona)
+
+; A Scheme form to determine the first element of mylist
+(define mylist '(4 8 15 16 23 42))
+
+; A Scheme form to determing the third element of mylist
+
